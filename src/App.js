1e887dc42a36fd8beba6001fbf26875d1e3ed79e
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2021-12-10 11:10:00 -0600

    getting array logged to console

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+//dependencies 
+import React, { useState, useEffect }from 'react';
+import axios from 'axios'; 
+//import { API_URL, API_KEY } from ""; 
+
+//Components 
+import Character from "./components/Character.js"; 
+
+
+//Stylesheets 
 import './App.css';
 
+
+
 const App = () => {
+
+  const [baseURL, setBaseURL] = useState("https://swapi.dev/api/people")
+  const [characterData, setCharacterData] = useState([])
+
+  useEffect(() => {
+    axios.get(baseURL)
+    .then((response) => {
+      console.log(response.data)
+      setCharacterData(response.data)
+    })
+  }, [])
+
+  console.log(characterData)
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,6 +36,12 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <h2>{characterData.name}</h2>
+      {
+        characterData.map((character, index) => (
+          <Character key={index} character={characterData} />
+        ))
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,28 @@
-// Write your Character component here
+ //Write your Character component here
+export default function Character(props) {
+    console.log(props)
+
+    return (
+        <article classNAme="character">
+            <h2>{props.Character.name}</h2>
+            <img src={props.character.name} alt={props.person.image} />
+        </article>
+    )
+}
+
+
+
+
+
+
+
+/* import React, { useState } from 'react'; 
+
+const Character = (props) => {
+   const { } = props; 
+    return (
+
+    );
+};
+
+export default Character; */
